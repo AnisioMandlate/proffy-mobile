e7fceff28c3ccf9d61e9b48ac66b7c6ec5e19afe
Anisio Mandlate <anisiomandlate@gmail.com>
2020-08-09 22:58:56 +0200

    feat: Display favorites teacher list

--- src/components/Favourites/index.tsx
+++ src/components/Favourites/index.tsx
@@ -1,10 +1,25 @@
-import React from "react";
-import { View, ScrollView } from "react-native";
+import React, { useState } from "react";
+import { View, ScrollView, AsyncStorage } from "react-native";
 import styles from "./styles";
 import PageHeader from "../PageHeader";
-import TeacherItem from "../TeacherItem";
+import TeacherItem, { Teacher } from "../TeacherItem";
+import { useFocusEffect } from "@react-navigation/native";
 
 function Favourites() {
+  const [favorites, setFavorites] = useState([]);
+  function loadFavorites() {
+    AsyncStorage.getItem("favorites").then((response) => {
+      if (response) {
+        const favoritedTeachers = JSON.parse(response);
+        setFavorites(favoritedTeachers);
+      }
+    });
+  }
+
+  useFocusEffect(() => {
+    loadFavorites();
+  });
+
   return (
     <View style={styles.container}>
       <PageHeader title="Meus Proffys Favoritos" />
@@ -12,12 +27,11 @@ function Favourites() {
         style={styles.teacherList}
         contentContainerStyle={{ paddingHorizontal: 16, paddingBottom: 16 }}
       >
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
+        {favorites.map((teacher: Teacher) => {
+          return (
+            <TeacherItem key={teacher.id} teacher={teacher} favorited={true} />
+          );
+        })}
       </ScrollView>
     </View>
   );
