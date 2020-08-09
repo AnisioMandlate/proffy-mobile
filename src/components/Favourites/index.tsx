46137266e81d49cd6d3718f491c94fe95841f95f
Anisio Mandlate <anisiomandlate@gmail.com>
2020-08-09 02:58:04 +0200

    feat: Add header to Teacher List page and add option to change color when the tab is focused or not

--- src/components/Favourites/index.tsx
+++ src/components/Favourites/index.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { View } from "react-native";
 import styles from "./styles";
+import PageHeader from "../PageHeader";
 
 function Favourites() {
-  return <View style={styles.container} />;
+  return (
+    <View style={styles.container}>
+      <PageHeader title="Meus Proffys Favoritos" />
+    </View>
+  );
 }
 
 export default Favourites;
--- src/components/TeacherList/index.tsx
+++ src/components/TeacherList/index.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import { View } from "react-native";
 import styles from "./styles";
+import PageHeader from "../PageHeader";
 
 function TeacherList() {
-  return <View style={styles.container} />;
+  return (
+    <View style={styles.container}>
+      <PageHeader
+        title="Proffys
+Disponíveis"
+      />
+    </View>
+  );
 }
 
 export default TeacherList;
