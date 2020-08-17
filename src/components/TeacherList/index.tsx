d66545dbf07b66736a095284edd712194a8e2372
Anisio Mandlate <anisiomandlate@gmail.com>
2020-08-17 18:37:37 +0200

    feat: Add the keyboard avoid view

--- src/components/TeacherList/index.tsx
+++ src/components/TeacherList/index.tsx
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
-import { View, ScrollView, Text, TextInput, AsyncStorage } from "react-native";
+import {
+  View,
+  ScrollView,
+  Text,
+  TextInput,
+  AsyncStorage,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
 import styles from "./styles";
 import PageHeader from "../PageHeader";
 import TeacherItem, { Teacher } from "../TeacherItem";
 import { BorderlessButton, RectButton } from "react-native-gesture-handler";
 import { Feather } from "@expo/vector-icons";
 import api from "../../services/api";
+import { Header } from "@react-navigation/stack";
 
 function TeacherList() {
   const [teachers, setTeachers] = useState([]);
@@ -47,70 +56,76 @@ function TeacherList() {
   }
 
   return (
-    <View style={styles.container}>
-      <PageHeader
-        title="Proffys
+    <KeyboardAvoidingView
+      keyboardVerticalOffset={Platform.select({ ios: 0, android: 500 })}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={{ flex: 1 }}
+    >
+      <View style={styles.container}>
+        <PageHeader
+          title="Proffys
 Disponíveis"
-        headerRight={
-          <BorderlessButton onPress={handleToggleFilterVisible}>
-            <Feather name="filter" size={20} color="#fff" />
-          </BorderlessButton>
-        }
-      >
-        {isFiltersVisible && (
-          <View style={styles.searchForm}>
-            <Text style={styles.label}>Matéria</Text>
-            <TextInput
-              style={styles.input}
-              value={subject}
-              onChangeText={(text) => setSubject(text)}
-              placeholder="Qual é a matéria? "
-              placeholderTextColor="#c1bccc"
-            />
-            <View style={styles.inputGroup}>
-              <View style={styles.inputBlock}>
-                <Text style={styles.label}>Dia da Semana</Text>
-                <TextInput
-                  style={styles.input}
-                  value={week_day}
-                  onChangeText={(text) => setWeek_day(text)}
-                  placeholder="Qual é o dia? "
-                  placeholderTextColor="#c1bccc"
-                />
+          headerRight={
+            <BorderlessButton onPress={handleToggleFilterVisible}>
+              <Feather name="filter" size={20} color="#fff" />
+            </BorderlessButton>
+          }
+        >
+          {isFiltersVisible && (
+            <View style={styles.searchForm}>
+              <Text style={styles.label}>Matéria</Text>
+              <TextInput
+                style={styles.input}
+                value={subject}
+                onChangeText={(text) => setSubject(text)}
+                placeholder="Qual é a matéria? "
+                placeholderTextColor="#c1bccc"
+              />
+              <View style={styles.inputGroup}>
+                <View style={styles.inputBlock}>
+                  <Text style={styles.label}>Dia da Semana</Text>
+                  <TextInput
+                    style={styles.input}
+                    value={week_day}
+                    onChangeText={(text) => setWeek_day(text)}
+                    placeholder="Qual é o dia? "
+                    placeholderTextColor="#c1bccc"
+                  />
+                </View>
+                <View style={styles.inputBlock}>
+                  <Text style={styles.label}>Horário</Text>
+                  <TextInput
+                    style={styles.input}
+                    value={time}
+                    onChangeText={(text) => setTime(text)}
+                    placeholder="Qual é o horário? "
+                    placeholderTextColor="#c1bccc"
+                  />
+                </View>
               </View>
-              <View style={styles.inputBlock}>
-                <Text style={styles.label}>Horário</Text>
-                <TextInput
-                  style={styles.input}
-                  value={time}
-                  onChangeText={(text) => setTime(text)}
-                  placeholder="Qual é o horário? "
-                  placeholderTextColor="#c1bccc"
-                />
-              </View>
+              <RectButton
+                onPress={handleFiltersSubmit}
+                style={styles.submitButton}
+              >
+                <Text style={styles.submitButtonText}>Filtrar</Text>
+              </RectButton>
             </View>
-            <RectButton
-              onPress={handleFiltersSubmit}
-              style={styles.submitButton}
-            >
-              <Text style={styles.submitButtonText}>Filtrar</Text>
-            </RectButton>
-          </View>
-        )}
-      </PageHeader>
-      <ScrollView
-        style={styles.teacherList}
-        contentContainerStyle={{ paddingHorizontal: 16, paddingBottom: 16 }}
-      >
-        {teachers.map((teacher: Teacher) => (
-          <TeacherItem
-            key={teacher.id}
-            teacher={teacher}
-            favorited={favorites.includes(teacher.id)}
-          />
-        ))}
-      </ScrollView>
-    </View>
+          )}
+        </PageHeader>
+        <ScrollView
+          style={styles.teacherList}
+          contentContainerStyle={{ paddingHorizontal: 16, paddingBottom: 16 }}
+        >
+          {teachers.map((teacher: Teacher) => (
+            <TeacherItem
+              key={teacher.id}
+              teacher={teacher}
+              favorited={favorites.includes(teacher.id)}
+            />
+          ))}
+        </ScrollView>
+      </View>
+    </KeyboardAvoidingView>
   );
 }
 
